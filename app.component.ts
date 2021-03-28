dcc687370bb2f4de05534a7fb60c598b3888a256
Satyananda Behera <66942568+satyasonygold@users.noreply.github.com>
2021-03-28 14:30:43 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'myproject';
+  submit()
+{
+  console.log("Submit Successful");
+}
+}
